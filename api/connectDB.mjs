7dbd98da60ca44d3e73a1dd7a42f3328d73352a7
Ritish Tejpal <ritishtejpal.bt21cse@pec.edu.in>
2023-07-23 03:35:54 +0530

    backend v1 complete

--- api/connectDB.mjs
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose';
-export async function connectDB(){
-    await mongoose.connect('mongodb://localhost:27017/Muzik_website', {useNewUrlParser: true, useUnifiedTopology: true});
-}
